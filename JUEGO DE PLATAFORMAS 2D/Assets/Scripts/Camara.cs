c2eb64771d8ef33832ca56a52b1e573ce820f00a
AlexisFarfan <jumpytunechi@gmail.com>
2021-12-19 02:22:30 +0100

    Creación de Tilemap
    Se ha añadido un tilemap y creado una nueva escena donde se han añadido al personaje ,cámara y fondo como prefab. tambien se ha mejorado la cámara y ahora sigue en x e y.

--- JUEGO DE PLATAFORMAS 2D/Assets/Scripts/Camara.cs
+++ JUEGO DE PLATAFORMAS 2D/Assets/Scripts/Camara.cs
@@ -13,11 +13,13 @@ public class Camara : MonoBehaviour
 
     public Transform background;
     private float lastXPos;
+    private float lastYPos;
 
     // Start is called before the first frame update
     void Start()
     {
         lastXPos = transform.position.x;
+        lastYPos = transform.position.y;
     }
 
     // Update is called once per frame
@@ -25,10 +27,12 @@ public class Camara : MonoBehaviour
     {
         //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
         // transform.position = new Vector3(playerPosition.position.x, 0, playerPosition.position.z - 10);
-        transform.position = new Vector3(target.position.x, target.position.y +2, target.position.z -1);
+        transform.position = new Vector3(target.position.x, target.position.y, target.position.z -1);
 
         float amountToMoveX = transform.position.x - lastXPos;
-        background.position += new Vector3(amountToMoveX, 0f, 0f);
+        float amonuntToMoveY = transform.position.y - lastYPos;
+        background.position += new Vector3(amountToMoveX, amonuntToMoveY, 0f);
         lastXPos = transform.position.x;
+        lastYPos = transform.position.y;
     }
 }
